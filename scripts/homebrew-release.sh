e6f90d4b36030cab9d5543e4563793ead9b04121
Matthew Warman <mcwarman@gmail.com>
2022-07-29 17:10:54 +0100

    ci: script tap release

---
+++ scripts/homebrew-release.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=${GITHUB_REF##*/}
+branch=bump-helm-to-hcl-${version}
+
+rm -rf ./dist/homebrew-tap
+
+git clone "https://bot:${GH_PUBLIC_REPO_TOKEN}@github.com/mcwarman/homebrew-tap.git" ./dist/homebrew-tap
+
+cd ./dist/homebrew-tap
+
+git checkout -b "${branch}"
+
+cp ../helm-to-hcl.rb ./Formula/helm-to-hcl.rb
+
+git commit -a -m "helm-to-hcl: Bump to ${version}"
+
+git push -u origin "${branch}"
+
+curl \
+   -d "{\"title\":\"helm-to-hcl: Bump to ${version}\",\"base\":\"main\", \"head\":\"${branch}\"}"  \
+   -H "Authorization: token ${GH_PUBLIC_REPO_TOKEN}" \
+  https://api.github.com/repos/mcwarman/homebrew-tap/pulls
